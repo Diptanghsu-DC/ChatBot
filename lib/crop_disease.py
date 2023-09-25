708ceace322696858e279a024783f2f33133d83a
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2023-09-26 00:52:30 +0530

    initial commit

---
+++ lib/crop_disease.py
@@ -0,0 +1,11 @@
+# # Define a function to handle the "crop disease prediction" intent
+# def handle_crop_disease_intent(params, session_id):
+#     # Extract parameters from 'params' dictionary
+#     crop_type = params.get('crop_type')
+#     location = params.get('location')
+
+#     # Implement your crop disease prediction logic here
+#     # You may use machine learning algorithms or a database
+
+#     # Return the crop disease prediction response
+#     return {"fulfillmentText": "No significant disease detected in {} for {}.".format(location, crop_type)}
